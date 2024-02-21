e2237484c024275b0eaf9a4b4c130180d62d302e
전병길 <jeonbg@kakao.com>
2024-02-21 21:24:52 +0900

    feat(settings): monorepo 환경 설정 * yarn berry * nx * eslint, prettier, * esbuild

--- packages/themes/build.js
+++ packages/themes/build.js
@@ -1,42 +1,6 @@
-import esbuild from "esbuild";
+import run from "@genie/esbuild-config";
 import pkg from "./package.json" assert { type: "json" };
 
-const isDevelopment = process.argv.includes("--dev");
-
-const minify = !isDevelopment;
-
-const isWatch = process.argv.includes("--watch");
-
-const external = Object.keys({
-  ...pkg.dependencies,
-  ...pkg.peerDependencies,
-});
-
-const baseConfig = {
-  entryPoints: ["src/index.ts"],
-  bundle: true,
-  minify,
-  sourcemap: true,
-  outdir: "dist",
-  watch: isWatch,
-  external,
-};
-
-Promise.all([
-  esbuild.build({
-    ...baseConfig,
-    format: "esm",
-    target: "es2019",
-  }),
-
-  esbuild.build({
-    ...baseConfig,
-    format: "cjs",
-    outExtension: {
-      ".js": ".cjs",
-    },
-  }),
-]).catch(() => {
-  console.error("Build Failed");
-  process.exit(1);
+run({
+  pkg,
 });
